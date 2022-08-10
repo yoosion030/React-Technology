b38a4f7e5d27c03d36b96e81572783a7f50cebb5
yoosion030 <rsy5487@naver.com>
2022-08-10 20:23:57 +0900

    Add life cycle

---
+++ life-cycle/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Average from "./Average";
+
+function App() {
+  return (
+    <div className="App">
+      <Average />
+    </div>
+  );
+}
+
+export default App;
