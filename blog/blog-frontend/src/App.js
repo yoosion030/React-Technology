8f5da1c517a1c190397c6f2bf89158ecbd57dd50
yoosion030 <rsy5487@naver.com>
2022-08-11 16:50:08 +0900

    Add API 연동

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<PostListPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/write" element={<WritePage />} />
+      <Route path="/@:username" element={<PostListPage />}>
+        <Route index element={<PostListPage />} />
+        <Route path=":postId" element={<PostPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -0,0 +1,12 @@
+import Button from '../components/common/Button';
+
+const PostListPage = () => {
+  return (
+    <div>
+      {' '}
+      <Button>버튼</Button>
+    </div>
+  );
+};
+
+export default PostListPage;
